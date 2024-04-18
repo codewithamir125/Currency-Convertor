a17d3d7c18dc33d424417d536b4121fdb93ca39e
codewithamir125 <waqasa152@gmail.com>
2024-04-18 12:40:05 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.rgb(204, 204, 204)(`<<<==========>>>  ${chalk.bold.hex('#9999FF')('Welcome To \'Code With Amir\' - Currency Converter')}  <<<===========>>>`));
+// Define the list of currencies and theire exchange rate
+let exchange_rate = {
+    "USD $": 1,
+    "EUR £": 0.88,
+    "JPY ¥": 113.32,
+    "CAD $": 1.29,
+    "AUD $": 1.44,
+    "PKR Rs": 277.70, // Pakistani (Rupee)
+};
+// Prompt the user to select currencies to convert from and to
+let user_answer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: "Select the currency you want to convert from:",
+        choices: ["USD $", "EUR £", "JPY ¥", "CAD $", "AUD $", "PKR Rs"],
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "Select the currency you want to convert into:",
+        choices: ["USD $", "EUR £", "JPY ¥", "CAD $", "AUD $", "PKR Rs"],
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "Enter the amount you want to convert:",
+    }
+]);
+// Perform currency conversions
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount;
+let base_amount = amount / from_amount; // USD Base Currency //
+let converted_amount = base_amount * to_amount;
+// Display the converted amount
+console.log(chalk.bold(`\n \t ${user_answer.from_currency}:${chalk.greenBright(amount)}  converted amount in  ${user_answer.to_currency}:${chalk.greenBright(converted_amount.toFixed(2))}`));
